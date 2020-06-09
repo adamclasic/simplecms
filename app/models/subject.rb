922433b46c37a7338e463e3fcf3eebcf35b1c660
adam <aabou9@gmail.com>
2020-06-09 10:50:19 +0100

    add scopes to subject model

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,11 @@
 class Subject < ApplicationRecord
 
+    scope :visible, lambda { where(:visible => true) }
+    scope :invisible, lambda { where(:visible => false) }
+    scope :sorted, lambda { order("position ASC") }
+    scope :sorted, lambda { order("created_at DESC") }
+    scope :search, lambda { |query| where(["name LIKE ?", "%#{query}%"]) }
+
     has_one :page
 
 end
